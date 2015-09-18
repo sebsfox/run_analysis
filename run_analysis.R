80eb691c5bf8a901f49aca43d9946fce1f50467c
sebsfox <sebsfox@users.noreply.github.com>
2015-09-18 11:31:35 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -18,22 +18,22 @@ test <- read.table("test//X_test.txt")
 testLabels <- read.table("test/y_test.txt")
 testSubject <- read.table("test/subject_test.txt")
 
-df <- rbind(train,test) #appends the test dataset to the train dataset
-dfActivities <- rbind(trainLabels,testLabels) #appends the test labels to the train labels
+df <- bind_rows(train,test) #appends the test dataset to the train dataset
+dfActivities <- bind_rows(trainLabels,testLabels) #appends the test labels to the train labels
 names(dfActivities) <- "activityID"
-dfSubjects <- rbind(trainSubject,testSubject) #appends the test subjects to the train subjects
+dfSubjects <- bind_rows(trainSubject,testSubject) #appends the test subjects to the train subjects
 names(dfSubjects) <- "subjectID"
 
 featuresText <- read.table("features.txt") #Import headers
 featuresText<- paste(featuresText[,1],featuresText[,2],sep=",")
 names(df) <- tolower(featuresText) #apply lower case names to data
-df <- cbind(select(df,contains("mean")),select(df,contains("std"))) #extracts only columns with mean and std in the name
+df <- bind_cols(select(df,contains("mean")),select(df,contains("std"))) #extracts only columns with mean and std in the name
 
 activityLabels <- read.table("activity_labels.txt") #import activity labels
 names(activityLabels) <- c("activityID","activityDescription")
 
 dfActivities <- left_join(dfActivities,activityLabels) #joins description from activityLabels to the ID of the activity
-df <- cbind(dfActivities,dfSubjects,df) #join activities to the main data frame
+df <- bind_cols(dfActivities,dfSubjects,df) #join activities to the main data frame
 dfTidy <- df %>%
         select(-activityID) %>%
         gather(variable,mean,-activityDescription,-subjectID) %>%
